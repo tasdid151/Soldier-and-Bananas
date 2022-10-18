05c746d32ee1ac0a478f2aecef61576aa1e1fa28
tasdid151 <109462135+tasdid151@users.noreply.github.com>
2022-10-18 20:27:55 +0600

    Add files via upload

---
+++ 546A_Soldier and Bananas.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int k, n, w, sum=0;
+    cin>>k>>n>>w;
+    for(int i=1; i<=w; i++)
+    {
+        sum=+i;
+    }
+    int d=(sum*k)-n;
+    if(d<0)
+    {
+        cout<<"0";
+    }
+    else
+    {
+        cout<<d;
+    }
+}
